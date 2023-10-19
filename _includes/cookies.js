a80c125bfd2bb0ba11c26ffc85bb6efb0bd29844
Taylor Hawkins <tjhawkins17@gmail.com>
2023-10-19 23:34:05 +0000

    analytics

--- _includes/cookies.js
+++ _includes/cookies.js
@@ -1,13 +1,14 @@
-
 (function() {
     const interactedWithCookiesBanner = "interacted_with_cookies_banner";
     const storageType = localStorage;
     const cookieContainerId = "cookieContainer";
+    
+    // Check if the user has previously interacted with the banner and consented
+    const userConsentedToAnalytics = storageType.getItem("GA") === "true"; // Set the user consent status
 
     acceptAllCookieConsent = () => {
-        {% for consent in site.data.metaData.cookies.consent %}
-            eval("storageType.setItem('{{consent.cookieName}}', true);");
-        {% endfor %}
+        eval("storageType.setItem('GA', true);");
+        userConsentedToAnalytics = true; // Set user consent status
     }
 
     window.cookiesAcceptDefaultFn = () => {
@@ -15,14 +16,19 @@
         cookieContainer.classList.add("hidden");
         window.setTimeout(cookieContainer.remove.bind(cookieContainer), 1000);
         storageType.setItem(interactedWithCookiesBanner, true);
-        acceptAllCookieConsent();
+        
+        // Check if user consented to Google Analytics and load it if consented
+        if (userConsentedToAnalytics) {
+            loadGoogleAnalytics(); // Function to load Google Analytics
+        }
     }
+
     window.cookiesDisagreeDefaultFn = () => {
         const cookieContainer = document.getElementById(cookieContainerId);
         cookieContainer.classList.add("hidden");
         window.setTimeout(cookieContainer.remove.bind(cookieContainer), 1000);
         storageType.setItem(interactedWithCookiesBanner, true);
-        window.location = `{{ "/cookies" | absolute_url }}`;
+        window.location = "{{ "/cookies" | absolute_url }}";
     }
 
     window.addEventListener("load", () => {
@@ -32,26 +38,26 @@
         const cookieContainer = document.createElement("div");
         cookieContainer.setAttribute("id", cookieContainerId);
         cookieContainer.classList.add("hidden");
-
+    
         const cookieHeader = document.createElement("h2");
         const cookieHeaderContent = document.createTextNode("{{ site.data.metaData.cookies.header }}");
-
+    
         const cookieParagraph = document.createElement("p");
         const cookieParagraphContent = document.createTextNode("{{ site.data.metaData.cookies.message }}");
-
+    
         const cookieButtonsContainer = document.createElement("div");
         cookieButtonsContainer.classList.add("cookie-container--buttons");
-
+    
         const cookieAgreeButton = document.createElement("button");
         const cookieButtonContent = document.createTextNode("{{ site.data.metaData.cookies.agreeButtonText }}");
         cookieAgreeButton.appendChild(cookieButtonContent);
-        eval("cookieAgreeButton.onclick = {{ site.data.metaData.cookies.agreeButtonFnName }};");
-
+        eval("cookieAgreeButton.onclick = cookiesAcceptDefaultFn;");
+    
         const cookieButtonDisagree = document.createElement("button");
         const cookieButtonDisagreeContent = document.createTextNode("{{ site.data.metaData.cookies.disagreeButtonText }}");
         cookieButtonDisagree.append(cookieButtonDisagreeContent);
-        eval("cookieButtonDisagree.onclick = {{ site.data.metaData.cookies.disagreeButtonFnName }};");
-
+        eval("cookieButtonDisagree.onclick = cookiesDisagreeDefaultFn;");
+    
         cookieHeader.append(cookieHeaderContent);
         cookieParagraph.append(cookieParagraphContent);
         cookieButtonsContainer.append(cookieAgreeButton, cookieButtonDisagree)
