58dcf09d31e4ac5bdabbbc4c3eb6591000e74723
Marcel Bensch <marcelbensch@gmail.com>
2024-07-20 00:28:46 -0500

    fix: potentially fix analytics code

--- _includes/analytics.js
+++ _includes/analytics.js
@@ -1,3 +1,11 @@
+// Check if the user has previously interacted with the banner and consented
+const userConsentedToAnalytics = storageType.getItem("GA") === "true"; // Set the user consent status
+
+// Check if user consented to Google Analytics and load it if consented
+if (userConsentedToAnalytics) {
+    loadGoogleAnalytics(); // Function to load Google Analytics
+}
+
 function loadGoogleAnalytics() {
     // Replace this with your actual Google Analytics tracking code
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -8,3 +16,10 @@ function loadGoogleAnalytics() {
     ga('create', 'G-P1F4L0XWG5', 'auto');
     ga('send', 'pageview');
 }
+
+window.addEventListener("storage", (event) => {
+    if (event.key === "GA" && event.newValue === "true") {
+        loadGoogleAnalytics();
+    }
+});
+
